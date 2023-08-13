d44d63db1815277237ead1189649d7a456328f45
i-seesharp <aaranya.barman@mail.utoronto.ca>
2023-08-13 16:16:30 -0700

    adding cicd path and component

--- src/testing-react-app/src/App.tsx
+++ src/testing-react-app/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './components/Home'
 import './App.css'
 import { About } from './components/About'
+import { CICD } from './components/CICD'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <Home /> } />
         <Route path='/about' element={ <About /> } />
+        <Route path='/cicd' element={ <CICD /> } />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/testing-react-app/src/components/CICD.tsx
@@ -0,0 +1,3 @@
+export const CICD = () => {
+    return <h1>CI/CD was successful</h1>
+}
